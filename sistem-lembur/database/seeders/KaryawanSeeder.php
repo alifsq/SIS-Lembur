3ab028a2bb97b5ea784f1c35b9a0f104feccfb82
alifsq <alifsq22@gmail.com>
2025-03-01 19:44:42 +0700

    menambahkan seeder

--- sistem-lembur/database/seeders/KaryawanSeeder.php
+++ sistem-lembur/database/seeders/KaryawanSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Karyawan;
+use App\Models\User;
 
 class KaryawanSeeder extends Seeder
 {
@@ -12,6 +14,48 @@ class KaryawanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'id' => 1,
+                'nama' => 'Budi',
+                'nip' => '13579',
+                'Posisi' => 'pegawai',
+                'email' => 'budi@example.com',
+                'password' => 'budi123',
+            ],
+        ];
+
+        $data2 = [
+            [
+                'id' => 1,
+                'name' => 'Budi',
+                'email' => 'budi@example.com',
+                'password' => 'budi123',
+                'role'=>'karyawan',
+            ],
+        ];
+
+        $data3 = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => 'admin123',
+                'role'=>'admin',
+            ]
+        ];
+
+        // Masukkan data ke dalam database
+        foreach ($data as $item) {
+            Karyawan::create($item);
+        }
+        
+        foreach ($data2 as $item) {
+            User::create($item);
+        }
+
+        foreach ($data3 as $item) {
+            User::create($item);
+        }
+
     }
 }
